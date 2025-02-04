07f62769a6edc04104b59d16afdcf1461a720589
textencoder <160430049+textencoder@users.noreply.github.com>
2025-02-04 15:34:48 -0600

    refactoring + full base support with decimals

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const resultContainer = document.getElementById("result");
 
-let firstNumber = '';
-let secondNumber = '';
+let firstNumber = "";
+let secondNumber = "";
 let operatorInput = null;
 let operatorHit = false;
 let result = null;
@@ -27,21 +27,14 @@ document.querySelector("#clear-btn").addEventListener("click", () => {
 document.querySelectorAll("#numbers button").forEach((button) => {
   button.addEventListener("click", () => {
     if (result && operatorHit) {
-      firstNumber = result;
-      secondNumber += button.value;
-      updateResultContainer(secondNumber);
-      console.log("second number", secondNumber);
+      chainEquation(button);
       return;
     }
 
     if (!operatorHit) {
-      firstNumber += button.value;
-      updateResultContainer(firstNumber);
-      console.log("first number", firstNumber);
+      addToFirst(button);
     } else if (operatorHit) {
-      secondNumber += button.value;
-      updateResultContainer(secondNumber);
-      console.log("second number", secondNumber);
+      addToSecond(button);
     }
   });
 });
@@ -57,44 +50,68 @@ document.querySelectorAll("#operators button:not(#equal)").forEach((button) => {
 
 document.querySelector("#equal").addEventListener("click", () => {
   if (firstNumber && secondNumber && operatorInput) {
-      if (Number(firstNumber) == 0 || Number(secondNumber) == 0) {
-        updateResultContainer('invalid');
-        resetValues();
-        return;
-      }
-      result = operate(Number(firstNumber), Number(secondNumber), operatorInput);
-      if (result % 1 == 0) {
-        updateResultContainer(result);
-      } else if (result % 1 != 0) {
-        result = Number(result.toFixed(2));
-        updateResultContainer(result);
-      }
-      console.log("result", result);
+    if (Number(firstNumber) == 0 || Number(secondNumber) == 0) {
+      updateResultContainer("invalid");
       resetValues();
+      return;
     }
+    result = operate(Number(firstNumber), Number(secondNumber), operatorInput);
+    if (result % 1 == 0) {
+      updateResultContainer(result);
+    } else if (result % 1 != 0) {
+      result = Number(result.toFixed(2));
+      updateResultContainer(result);
+    }
+    console.log("result", result);
+    resetValues();
   }
-);
+});
 
 function updateResultContainer(num) {
   resultContainer.textContent = num;
 }
 
 function resetValues() {
-      firstNumber = '';
-      secondNumber = '';
-      operatorInput = null;
-      operatorHit = false;
-      enableOperators();
+  firstNumber = "";
+  secondNumber = "";
+  operatorInput = null;
+  operatorHit = false;
+  enableOperators();
 }
 
 function disableOperators() {
-  document.querySelectorAll("#operators button:not(#equal)").forEach(button => {
-    button.disabled = true;
-  })
+  document
+    .querySelectorAll("#operators button:not(#equal)")
+    .forEach((button) => {
+      button.disabled = true;
+      button.style.backgroundColor = "#A5A5A5";
+    });
 }
 
 function enableOperators() {
-  document.querySelectorAll("#operators button:not(#equal)").forEach(button => {
-    button.disabled = false;
-  })
+  document
+    .querySelectorAll("#operators button:not(#equal)")
+    .forEach((button) => {
+      button.disabled = false;
+      button.style.backgroundColor = "#FE9505";
+    });
+}
+
+function chainEquation(button) {
+  firstNumber = result;
+  secondNumber += button.value;
+  updateResultContainer(secondNumber);
+  console.log("second number", secondNumber);
+}
+
+function addToFirst(button) {
+  firstNumber += button.value;
+  updateResultContainer(firstNumber);
+  console.log("first number", firstNumber);
+}
+
+function addToSecond(button) {
+  secondNumber += button.value;
+  updateResultContainer(secondNumber);
+  console.log("second number", secondNumber);
 }
