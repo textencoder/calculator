be9c05becbac44da05ac64e4a67a8e9032a9fd3c
textencoder <160430049+textencoder@users.noreply.github.com>
2025-02-03 20:12:24 -0600

    calc 0.0.3
    - added independent function to update result container
    - handled some edge cases
    - calculator accepts equation chaining
    
    known bugs
    
    - using zero in an equation not allowed
    - input doesn't accept numbers longer than one digit
    - decimal button does not have any function
    - clicking number button after a successful equation doesn't replace result

--- script.js
+++ script.js
@@ -1,53 +1,78 @@
-const resultContainer = document.getElementById('result');
+const resultContainer = document.getElementById("result");
 
-let firstNumber;
-let secondNumber;
-let operatorInput;
+let firstNumber = null;
+let secondNumber = null;
+let operatorInput = null;
+let result = null;
 
 function operate(a, b, operator) {
-     const operations = {
-         divide: a / b,
-         multiply: a * b,
-         subtract: a - b,
-         add: a + b,
-     }
+  const operations = {
+    divide: a / b,
+    multiply: a * b,
+    subtract: a - b,
+    add: a + b,
+  };
 
-    return operations[operator];
+  return operations[operator];
 }
 
-document.querySelector('#clear-btn').addEventListener('click', () => {
-    resultContainer.textContent = 0;
-})
+document.querySelector("#clear-btn").addEventListener("click", () => {
+  firstNumber = null;
+  secondNumber = null;
+  result = null;
+  updateResultContainer(0);
+});
 
-document.querySelectorAll('#numbers button').forEach(button => {
+document.querySelectorAll("#numbers button").forEach((button) => {
+  button.addEventListener("click", () => {
+    if (result) {
+      firstNumber = result;
+      secondNumber = Number(button.value);
+      updateResultContainer(secondNumber);
+      return;
+    } else if (!firstNumber) {
+      firstNumber = Number(button.value);
+      updateResultContainer(firstNumber);
+      console.log("first number", firstNumber);
+    }
 
-    button.addEventListener('click', () => {
-        if (!firstNumber) {
-            firstNumber = Number(button.value);
-            resultContainer.textContent = firstNumber;
-            console.log('first number', firstNumber);
-        } else if (!secondNumber) {
-            secondNumber = Number(button.value);
-            resultContainer.textContent = secondNumber;
-            console.log('second number', secondNumber);
-        }
-    })
-    
-})
+    if (firstNumber && operatorInput && !secondNumber) {
+      secondNumber = Number(button.value);
+      updateResultContainer(secondNumber);
+      console.log("second number", secondNumber);
+    }
+  });
+});
 
-document.querySelectorAll("#operators button:not(#equal)").forEach(button => {
-    button.addEventListener('click', () => {
-        operatorInput = button.id;
-        console.log(operatorInput);
-    })
-    
-})
+document.querySelectorAll("#operators button:not(#equal)").forEach((button) => {
+  button.addEventListener("click", () => {
+    operatorInput = button.id;
+    console.log(operatorInput);
+  });
+});
 
-document.querySelector('#equal').addEventListener('click', () => {
-    
-    let result = operate(firstNumber, secondNumber, operatorInput);
-    resultContainer.textContent = result;
-    console.log(result);
-    firstNumber = null;
-    secondNumber = null;
-})
+document.querySelector("#equal").addEventListener("click", () => {
+  if (firstNumber && secondNumber && operatorInput) {
+    if (firstNumber == 0 && operatorInput == "multiply") {
+      result = 0;
+      updateResultContainer(result);
+      console.log(result);
+    } else {
+      result = operate(firstNumber, secondNumber, operatorInput);
+      if (result % 1 == 0) {
+        updateResultContainer(result);
+      } else if (result % 1 != 0) {
+        result = Number(result.toFixed(2));
+        updateResultContainer(result);
+      }
+      console.log("result", result);
+      firstNumber = null;
+      secondNumber = null;
+      operatorInput = null;
+    }
+  }
+});
+
+function updateResultContainer(num) {
+  resultContainer.textContent = num;
+}
